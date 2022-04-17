a09746cefb74fdea698272ba09994ee7ac93134d
Daniel Miranda <daniel.sousa@trinto.com.br>
2022-04-17 18:43:22 -0300

    add database with gorm

--- main.go
+++ main.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"api-rest-go/models"
+	"api-rest-go/database"
 	"api-rest-go/routes"
 	"fmt"
 )
 
 func main() {
-	models.Personalidades = []models.Personalidade{
-		{Id: 1, Nome: "Nome 1", Historia: "História 1"},
-		{Id: 2, Nome: "Nome 2", Historia: "História 2"},
-	}
+
+	database.ConnectaComBancoDeDados()
 
 	fmt.Println("Iniciando o servidor Rest...")
 
